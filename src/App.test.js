d07552267506f283a4e4bdf1652c8b1dacb254f6
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-06 00:55:59 +0530

    unit(test):for app component renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders Tracker Board', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Tracker Board/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Landing App", () => {
+  test("renders correctly", () => {
+    render(<App />);
+    expect(
+      screen.getByRole("heading", {
+        name: "Track your expenses! 💸",
+      })
+    ).toBeInTheDocument();
+    const paraElement = screen.getByText(
+      /keeping track of your expenses is an important part of managing your overall finances\./i
+    );
+    expect(paraElement).toBeInTheDocument();
+  });
 });
